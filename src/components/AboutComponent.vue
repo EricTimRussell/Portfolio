05ae20c4edc88436e047ac17b8d3309c5b2afe28
Eric Russell <Erictim.Russell@live.com>
2023-03-25 22:24:26 -0600

    codeworks logo

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,12 +1,17 @@
 <template>
   <section id="about" class="container-fluid">
-    <div class="row mt-5">
+    <div class="row mt-5 justify-content-center">
       <div class="col-12 d-flex flex-column align-items-center">
-        <h2 class="text-retro fs-xl d-flex">ABOUT</h2>
+        <h2 class="text-retro fs-xl d-flex letter-space">ABOUT ME</h2>
         <span class="divider"></span>
       </div>
-      <div class="col-12 text-retro fs-md py-3 letter-space">
-        <p class="paragraph text-shadow"> My career life began when I graduated from Boise State University with a BS in
+      <div class="col-4 pt-5">
+        <img class="px-5 code-works-logo"
+          src="https://codeworks.blob.core.windows.net/public/assets/img/cw-logo-circle.png" alt="Boise CodeWorks Logo">
+      </div>
+      <div class="col-6 text-retro fs-md py-5 my-5 letter-space">
+        <p class="paragraph text-shadow"> &nbsp&nbsp My career life began when I graduated from Boise State University
+          with a BS in
           criminal
           justice. Shortly
           after graduating I learned that the world of law enforcement was not for me. I found myself working in the
@@ -30,4 +35,8 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.code-works-logo {
+  box-shadow: #33ff00 5px 5px 5px 0px, #33ff00 0px 0px 5px 5px;
+}
+</style>
