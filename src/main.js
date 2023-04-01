a40dca0fba4543b0bcbd26c5a682e105e96e0416
Eric Russell <Erictim.Russell@live.com>
2023-04-01 08:59:26 -0600

    remove firebase

--- src/main.js
+++ src/main.js
@@ -2,21 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { VueFire } from 'vuefire'
-import { firebaseApp } from "@/firebase.js"
-
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
 import './assets/main.css'
 
 const app = createApp(App)
-
-app
-  .use(VueFire, {
-    firebaseApp,
-    modules: []
-  })
 
 
 app.use(router)
